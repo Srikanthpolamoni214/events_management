71df5b869d3e2babc1b862b73cfeff65532b78ea
Srikanthpolamoni <polamonisrikanth123@gmail.com>
2025-02-25 22:53:48 +0530

    Implement search functionality with suggestions and results display

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
-import {auth, createUserWithEmailAndPassword,signInWithEmailAndPassword } from './firebase.js';
+import {
+  auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "./firebase.js";
 
 let signUp = document.getElementById("signUp");
-let cd = document.getElementsByClassName('.cd');
+let cd = document.getElementsByClassName(".cd");
 
 signUp.addEventListener("submit", function (e) {
-    e.preventDefault();
-    let mail = e.target[1].value;
-    let psd = e.target[2].value;
-  
-    
-    createUserWithEmailAndPassword(auth, mail, psd)  
-    .then((x)=>{
-        if(x.user){
-            alert("account created successfully");
-            location.replace("login.html");
-        }
-    })
+  e.preventDefault();
+  let mail = e.target[1].value;
+  let psd = e.target[2].value;
 
+  createUserWithEmailAndPassword(auth, mail, psd).then((x) => {
+    if (x.user) {
+      alert("account created successfully");
+      location.replace("login.html");
+    } else {
+      alert("pass");
+    }
+  });
 });
 let login = document.getElementById("login");
 
-login.addEventListener("submit",function(e){
-    e.preventDefault();
-    let mail = e.target[0].value;
-    let psd = e.target[1].value;
-    console.log(mail,psd);
-    location.replace("home.html");
-    // signInWithEmailAndPassword(auth, mail, psd)
-    // .then((x)=>{
-    //     if(x.user.accessToken){
-    //         alert("login successfully");
-    //         location.replace("home.html");
-    //     }
-    // })
-    // .catch((err)=>{
-    //     alert(err.message);
-    // })
-})
-
-
+login.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let mail = e.target[0].value;
+  let psd = e.target[1].value;
+  console.log(mail, psd);
+  location.replace("home.html");
+  // signInWithEmailAndPassword(auth, mail, psd)
+  // .then((x)=>{
+  //     if(x.user.accessToken){
+  //         alert("login successfully");
+  //         location.replace("home.html");
+  //     }
+  // })
+  // .catch((err)=>{
+  //     alert(err.message);
+  // })
+});
